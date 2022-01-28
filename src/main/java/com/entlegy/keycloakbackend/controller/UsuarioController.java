4dbc8c0fbce5117e144dff00cf62878b92155f16
mgualdron73 <marlon.gualdron@entelgy.com>
2022-01-28 12:55:16 -0500

    version 28 ene 2022

--- src/main/java/com/entlegy/keycloakbackend/controller/UsuarioController.java
+++ src/main/java/com/entlegy/keycloakbackend/controller/UsuarioController.java
@@ -1,12 +1,22 @@
 package com.entlegy.keycloakbackend.controller;
 
+import java.util.List;
+
+import javax.ws.rs.Produces;
+
 import com.entlegy.keycloakbackend.dto.ResponseMessage;
 import com.entlegy.keycloakbackend.model.Usuarios;
 import com.entlegy.keycloakbackend.service.KeycloakService;
 
+import org.keycloak.representations.idm.UserRepresentation;
+import org.keycloak.representations.idm.ClientRepresentation;
+import org.keycloak.representations.idm.RealmEventsConfigRepresentation;
+import org.keycloak.representations.idm.RoleRepresentation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,4 +38,36 @@ public class UsuarioController {
         return ResponseEntity.status(status).body(message);
     }
 
+    @GetMapping("/listRealm")
+    @Produces(value = "application/json")
+    public RealmEventsConfigRepresentation getRealms() {
+        RealmEventsConfigRepresentation rr = keycloakService.getListRealms();
+        // String res = rr.toString();
+        return rr;
+    }
+
+    @GetMapping("/rolesDisponibles")
+    @Produces(value = "application/json")
+    public List<RoleRepresentation> getRolesDisponibles() {
+        List<RoleRepresentation> rolesDisponibles = keycloakService.getRolesDisponibles();
+        return rolesDisponibles;
+    }
+
+    @GetMapping("/clientesDisponibles")
+    @Produces(value = "application/json")
+    public List<ClientRepresentation> getClientesDisponibles() {
+        return keycloakService.getClientesDisponibles();
+    }
+
+    @GetMapping("/usuariosDisponibles")
+    @Produces(value = "application/json")
+    public List<UserRepresentation> getUsuariosDisponibles() {
+        return keycloakService.getUserRealm();
+    }
+
+    @GetMapping("/clienteByUsuario/{userName}")
+    @Produces(value = "application/json")
+    public List<ClientRepresentation> getClientebyUsuario(@PathVariable("userName") String nameUser) {
+        return keycloakService.getClientbyUser(nameUser);
+    }
 }
--- src/main/java/com/entlegy/keycloakbackend/service/KeycloakService.java
+++ src/main/java/com/entlegy/keycloakbackend/service/KeycloakService.java
@@ -1,6 +1,7 @@
 package com.entlegy.keycloakbackend.service;
 
 import java.util.Arrays;
+import java.util.List;
 
 import javax.ws.rs.core.Response;
 
@@ -12,7 +13,10 @@ import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.KeycloakBuilder;
 import org.keycloak.admin.client.resource.RealmResource;
 import org.keycloak.admin.client.resource.UsersResource;
+import org.keycloak.admin.client.token.TokenManager;
+import org.keycloak.representations.idm.ClientRepresentation;
 import org.keycloak.representations.idm.CredentialRepresentation;
+import org.keycloak.representations.idm.RealmEventsConfigRepresentation;
 import org.keycloak.representations.idm.RoleRepresentation;
 import org.keycloak.representations.idm.UserRepresentation;
 import org.springframework.beans.factory.annotation.Value;
@@ -73,8 +77,92 @@ public class KeycloakService {
         return kc.realm(realm);
     }
 
+    private RealmResource getRealmResource(String realmSeek, String accesToken) {
+        Keycloak kc = KeycloakBuilder.builder().serverUrl(serverURL).realm(realmSeek).username("admin")
+                .password("admin").clientId("admin-cli").authorization(accesToken)
+                .resteasyClient(new ResteasyClientBuilder().connectionPoolSize(10).build())
+                .build();
+        return kc.realm(realm);
+    }
+
     private UsersResource getUsersResource() {
         RealmResource realmResource = getRealmResource();
         return realmResource.users();
     }
+
+    public RealmEventsConfigRepresentation getListRealms() {
+        // List<RealmResource> resultado = null;
+        RealmResource rr = getRealmResource("tutorial", getTokenAccess());
+        RealmEventsConfigRepresentation realmEventsConfig = rr.getRealmEventsConfig();
+        return realmEventsConfig;
+    }
+
+    private String getTokenAccess() {
+        Keycloak kc = KeycloakBuilder.builder().serverUrl(serverURL).realm("master").username("admin")
+                .password("admin").clientId("admin-cli").grantType("password")
+                .resteasyClient(new ResteasyClientBuilder().connectionPoolSize(10).build())
+                .build();
+        TokenManager tokenManager = kc.tokenManager();
+        return tokenManager.getAccessTokenString();
+    }
+
+    private String getUserId(String usuario) {
+        String p = getTokenAccess();
+        RealmResource rr = getRealmResource("tutorial", p);
+        List<UserRepresentation> lstUsers = rr.users().list();
+        boolean sw = false;
+        int i = 0;
+        int indice = -1;
+        while (i < lstUsers.size() && !sw) {
+            if (lstUsers.get(i).getUsername().equals(usuario)) {
+                indice = i;
+                sw = true;
+            }
+            i++;
+        }
+        if (indice == -1) {
+            return "";
+        } else {
+            return lstUsers.get(indice).getId();
+        }
+    }
+
+    public List<UserRepresentation> getUserRealm() {
+        String p = getTokenAccess();
+        RealmResource rr = getRealmResource("tutorial", p);
+        return rr.users().list();
+    }
+
+    public List<RoleRepresentation> getRolesDisponibles() {
+        String p = getTokenAccess();
+        RealmResource rr = getRealmResource("tutorial", p);
+        return rr.roles().list();
+    }
+
+    public List<ClientRepresentation> getClientesDisponibles() {
+        String p = getTokenAccess();
+        RealmResource rr = getRealmResource("tutorial", p);
+        return rr.clients().findAll();
+    }
+
+    public List<ClientRepresentation> getClientbyUser(String usuario) {
+        String p = getTokenAccess();
+
+        String userId = getUserId(usuario);
+        String url = serverURL + "/tutorial/groups/" + userId
+                + "/role-mappings/clients/tutorial-frontend/available";
+        Keycloak kc = KeycloakBuilder.builder().serverUrl(url).username("admin")
+                .password("admin")
+                .clientId("admin-cli").authorization(p)
+                .resteasyClient(new ResteasyClientBuilder().connectionPoolSize(10).build())
+                .build();
+        RealmResource rr = kc.realm("tutorial");
+
+        if (!userId.isEmpty()) {
+            return rr.clients().findAll();
+        } else {
+            return null;
+        }
+    }
+
 }
